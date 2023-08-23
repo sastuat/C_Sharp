2791e79369474d83e3d8780a6723430e17569db2
Yurij <>
2023-08-23 22:16:16 +0300

    task7

---
+++ task007/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите трехзначное число");
+
+string a = Console.ReadLine();
+if (Convert.ToInt32(a) > 999)
+{
+   Console.WriteLine("Число не трехзначное"); 
+}
+else 
+{
+Console.WriteLine("Последний символ: " +a[2]);
+}
+
+    
+
